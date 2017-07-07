c45a3ac0e194a5badd5636e056181e22cab07ffb
Lenik <pub.lenik@bodz.net>
2017-07-07 08:55:14 +0800

    Moved statcounter to site-supp.js.

---
+++ src/site-supp.js
@@ -0,0 +1,7 @@
+var sc_project = 11389146;
+var sc_invisible = 0;
+var sc_security = "cb1bef8f";
+
+var is_https = "https:" == document.location.protocol;
+var sc_url = (is_https ? "https://secure." : "http://www.") + "statcounter.com/counter/counter.js";
+var sc_script = "<script type='text/javascript' src='" + sc_url + "'></script>";
